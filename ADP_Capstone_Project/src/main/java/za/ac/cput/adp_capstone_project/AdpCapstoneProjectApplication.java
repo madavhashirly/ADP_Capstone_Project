c57650160b6536abbbc2ac60949499acc9fdc70c
Mogammad Mas'ood Lamera <128643400+mmlamera@users.noreply.github.com>
2024-05-15 22:45:07 +0200

    Add files via upload
    base packages

---
+++ ADP_Capstone_Project/src/main/java/za/ac/cput/adp_capstone_project/AdpCapstoneProjectApplication.java
@@ -0,0 +1,13 @@
+package za.ac.cput.adp_capstone_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdpCapstoneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdpCapstoneProjectApplication.class, args);
+	}
+
+}
---
+++ ADP_Capstone_Project/src/test/java/za/ac/cput/adp_capstone_project/AdpCapstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package za.ac.cput.adp_capstone_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdpCapstoneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
